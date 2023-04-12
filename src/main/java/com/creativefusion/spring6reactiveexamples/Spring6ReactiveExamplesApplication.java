7a1bdd0ccb12da9f17e1d4848a1543c26cf443c5
sadat.malik <sadat.malik@me.com>
2023-04-12 23:32:47 +0100

    initial project files

---
+++ src/main/java/com/creativefusion/spring6reactiveexamples/Spring6ReactiveExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.creativefusion.spring6reactiveexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6ReactiveExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6ReactiveExamplesApplication.class, args);
+    }
+
+}
