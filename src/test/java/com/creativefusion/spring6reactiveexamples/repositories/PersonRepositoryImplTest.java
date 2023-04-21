3aa09aaa096b6a37c8e79f9976435c54be219e6a
sadat.malik <sadat.malik@me.com>
2023-04-21 09:02:55 +0100

    adding step verifier unit test examples

--- src/test/java/com/creativefusion/spring6reactiveexamples/repositories/PersonRepositoryImplTest.java
+++ src/test/java/com/creativefusion/spring6reactiveexamples/repositories/PersonRepositoryImplTest.java
@@ -4,6 +4,7 @@ import com.creativefusion.spring6reactiveexamples.domain.Person;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import java.util.List;
 import java.util.Objects;
@@ -45,6 +46,27 @@ class PersonRepositoryImplTest {
         assertNotEquals(Boolean.TRUE, personMono.hasElement().block());
     }
 
+    @Test
+    void testGetByIdFoundStepVerifier() {
+        Mono<Person> personMono = personRepository.getById(3);
+
+        StepVerifier.create(personMono)
+                .expectNextCount(1)
+                .verifyComplete();
+
+        personMono.subscribe(person -> System.out.println(person.getFirstName()));
+    }
+
+    @Test
+    void testGetByIdNotFoundStepVerifier() {
+        Mono<Person> personMono = personRepository.getById(6);
+
+        StepVerifier.create(personMono)
+                .expectNextCount(0)
+                .verifyComplete();
+
+        personMono.subscribe(person -> System.out.println(person.getFirstName()));
+    }
 
     @Test
     void testMapOperation() {
